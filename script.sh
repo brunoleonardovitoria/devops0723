b94e893e54dde1282ac6fc06b15109776ac5a404
vagrant <vagrant@controle>
2023-08-14 03:41:29 +0300

    Commit Inicial

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cd /tmp
+wget https://apt.puppet.com/puppet7-release-bullseye.deb
+dpkg -i puppet7-release-bullseye.deb
 apt-get update
-apt-get upgrade -y
-apt-get install vim -y
+#apt-get upgrade -y
+apt-get install vim puppet-agent -y
